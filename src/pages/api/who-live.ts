6cf814f0f02c58b215c841e227bf16670ea0391c
motherofclamperl <motherofclamperl@gmail.com>
2024-08-08 19:47:30 -0700

    almost working again

--- src/pages/api/who-live.ts
+++ src/pages/api/who-live.ts
@@ -1,66 +1,99 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import {Browser, firefox} from "@playwright/test";
 
-function tagList() {
+//@ts-ignore
+if (!global.terriTags) global.terriTags = {"_itzpsyiconic_": false}; //@ts-ignore
+if (!global.spinoffTags) global.spinoffTags = {}; //@ts-ignore
+if (!global.browser) firefox.launch().then((browser: Browser) => {
+	//@ts-ignore
+	global.browser = browser;
+	afterBrowserLaunched();
+});
+
+function fetchTags(cb: (tagData: string) => void) {
 	try {
 		return fetch(
 			"https://raw.githubusercontent.com/MotherOfClamperl/terriverse/main/list.txt"
 		)
 			.then((res) => res.text())
-			.then((txt) => txt.split("\n\n")[0].split("\n"))
+			.then((txt) => {
+				cb(txt)
+			})
 			.catch((error) => console.warn(error));
 	} catch (err) {
-		console.error("Error fetching tags:", err);
-		return new Promise((resolve, reject) => {
-			resolve(["_itzpsyiconic_"]); // at least return main tag
-		}) as Promise<string[]>;
+		console.warn('error fetching ' + (err as Error).message);
 	}
+}
+function afterBrowserLaunched() {
+	//@ts-ignore
+	if (!global.tagData) fetchTags((tagData) => {
+		//@ts-ignore
+		global.tagData = tagData;//@ts-ignore
+		const terriAndSpinoffTags = tagData.split("\n\n");
+		const terriTags = terriAndSpinoffTags[0].split("\n");
+		const spinoffTags = terriAndSpinoffTags[1].split("\n");
+		if (spinoffTags[spinoffTags.length - 1] === "") spinoffTags.pop()
+		const startingTerriStates = new Array(terriTags.length).fill(false); //just assume everyone is offline until we can process the tag
+		const startingSpinoffStates = new Array(spinoffTags.length).fill(false); //just assume everyone is offline until we can process the tag
+		//@ts-ignore
+		global.terriTags = Object.fromEntries(terriTags.map((e, i) => [e, startingTerriStates[i]])); //@ts-ignore
+		global.spinoffTags = Object.fromEntries(spinoffTags.map((e, i) => [e, startingSpinoffStates[i]]));
+		startProcessingTags();
+	});
+	else startProcessingTags();
+}
+
+function startProcessingTags() {
+	console.log(`lets process some tags`);
 }
 
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-	tagList().then((tags: string[] | void) => {
-		if (!tags) return res.status(200).json({ err: "no tag list" });
-		async function tagCheck(tag: string) {
-			const page = await browser.newPage();
-			try {
-				await page.goto("https://tiktok.com/@" + tag);
-			} catch(e) {
-				return false;
-			}
-			const links = await page.getByRole("link").all();
-			console.log(`lennn ${links.length}`);
-			for (let link of links)
-				if ((await link.getAttribute("target")) === "tiktok_live_view_window")
-					return true;
-			return false;
-		}
-		// tags.push("XXX"); // test: uncomment line, replace XXX with tag of currently live user (it will be the sole "true" response)
-		let browser: Browser;
-		firefox.launch().then((_browser: Browser) => {
-			browser = _browser;
-			const tagInfo: { [key: string]: boolean } = {};
-			let tagCount = tags.length;
-			for (let i in tags) {
-				const tag = tags[i];
-				if (!tag) {
-					tagCount--;
-					continue;
-				} //ignore blank lines
-				tagCheck(tag)
-					.then((res) => {
-						tagInfo[tag] = res;
-						console.log(`${Object.keys(tagInfo).length}===${tagCount}`);
-						if (Object.keys(tagInfo).length === tagCount) console.log(tagInfo);
-						if (Object.keys(tagInfo).length === tagCount) finishResponse().then((res) => {});
-					})
-			}
-			async function finishResponse() {
-				res.status(200).json({
-					tagInfo,
-				});
-				await browser.close();
-			}
-		});
+	res.status(200).json({ //@ts-ignore
+		"tagInfo": global.terriTags,
 	});
+	// tagList().then((tags: string[] | void) => {
+	// 	if (!tags) return res.status(200).json({ err: "no tag list" });
+	// 	async function tagCheck(tag: string) {
+	// 		const page = await browser.newPage();
+	// 		try {
+	// 			await page.goto("https://tiktok.com/@" + tag);
+	// 		} catch(e) {
+	// 			return false;
+	// 		}
+	// 		const links = await page.getByRole("link").all();
+	// 		console.log(`lennn ${links.length}`);
+	// 		for (let link of links)
+	// 			if ((await link.getAttribute("target")) === "tiktok_live_view_window")
+	// 				return true;
+	// 		return false;
+	// 	}
+	// 	// tags.push("XXX"); // test: uncomment line, replace XXX with tag of currently live user (it will be the sole "true" response)
+	// 	let browser: Browser;
+	// 	firefox.launch().then((_browser: Browser) => {
+	// 		browser = _browser;
+	// 		const tagInfo: { [key: string]: boolean } = {};
+	// 		let tagCount = tags.length;
+	// 		for (let i in tags) {
+	// 			const tag = tags[i];
+	// 			if (!tag) {
+	// 				tagCount--;
+	// 				continue;
+	// 			} //ignore blank lines
+	// 			tagCheck(tag)
+	// 				.then((res) => {
+	// 					tagInfo[tag] = res;
+	// 					console.log(`${Object.keys(tagInfo).length}===${tagCount}`);
+	// 					if (Object.keys(tagInfo).length === tagCount) console.log(tagInfo);
+	// 					if (Object.keys(tagInfo).length === tagCount) finishResponse().then((res) => {});
+	// 				})
+	// 		}
+	// 		async function finishResponse() {
+	// 			res.status(200).json({
+	// 				tagInfo,
+	// 			});
+	// 			await browser.close();
+	// 		}
+	// 	});
+	// });
 }
--- src/pages/api/who-live/spinoffs.ts
+++ src/pages/api/who-live/spinoffs.ts
@@ -1,56 +1,61 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
-function tagList() {
-	try {
-		return fetch(
-			"https://raw.githubusercontent.com/MotherOfClamperl/terriverse/main/list.txt",
-			{ cache: "no-store" }
-		)
-			.then((res) => res.text())
-			.then((txt) => txt.split("\n\n")[1].split("\n"))
-			.catch((error) => console.warn(error));
-	} catch (err) {
-		console.error("Error fetching tags:", err);
-		return new Promise((resolve, reject) => {
-			resolve(["prettyaxme", "prettyaxme2"]); // at least return some main characters
-		}) as Promise<string[]>;
-	}
-}
-function tagCheck(tag: string) {
-	//check if user tag is live (html contains SpanLiveBadge)
-	try {
-		return fetch("https://tiktok.com/@" + tag)
-			.then((res) => res.text())
-			.then((res) => res.split("SpanLiveBadge").length - 1 > 0);
-	} catch (err) {
-		console.error("Error fetch checking tag: " + tag, err);
-		return new Promise((resolve, reject) => {
-			resolve(false); // don't retry, just pass false
-		}) as Promise<boolean>;
-	}
-}
-
+// function tagList() {
+// 	try {
+// 		return fetch(
+// 			"https://raw.githubusercontent.com/MotherOfClamperl/terriverse/main/list.txt",
+// 			{ cache: "no-store" }
+// 		)
+// 			.then((res) => res.text())
+// 			.then((txt) => txt.split("\n\n")[1].split("\n"))
+// 			.catch((error) => console.warn(error));
+// 	} catch (err) {
+// 		console.error("Error fetching tags:", err);
+// 		return new Promise((resolve, reject) => {
+// 			resolve(["prettyaxme", "prettyaxme2"]); // at least return some main characters
+// 		}) as Promise<string[]>;
+// 	}
+// }
+// function tagCheck(tag: string) {
+// 	//check if user tag is live (html contains SpanLiveBadge)
+// 	try {
+// 		return fetch("https://tiktok.com/@" + tag)
+// 			.then((res) => res.text())
+// 			.then((res) => res.split("SpanLiveBadge").length - 1 > 0);
+// 	} catch (err) {
+// 		console.error("Error fetch checking tag: " + tag, err);
+// 		return new Promise((resolve, reject) => {
+// 			resolve(false); // don't retry, just pass false
+// 		}) as Promise<boolean>;
+// 	}
+// }
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-	tagList().then((tags: string[] | void) => {
-		if (!tags) return res.status(200).json({ err: "no tag list" });
-		// tags.push("XXX"); // test: uncomment line, replace XXX with tag of currently live user (it will be the sole "true" response)
-		function finishResponse() {
-			res.status(200).json({
-				tagInfo,
-			});
-		}
-		const tagInfo: { [key: string]: boolean } = {};
-		let tagCount = tags.length;
-		for (let i in tags) {
-			const tag = tags[i];
-			if (!tag) {
-				tagCount--;
-				continue;
-			} //ignore blank lines
-			tagCheck(tag).then((res) => {
-				tagInfo[tag] = res;
-				if (Object.keys(tagInfo).length === tagCount) finishResponse();
-			});
-		}
+	res.status(200).json({ //@ts-ignore
+		"tagInfo": global.spinoffTags,
 	});
 }
+//
+// export default function handler(req: NextApiRequest, res: NextApiResponse) {
+// 	tagList().then((tags: string[] | void) => {
+// 		if (!tags) return res.status(200).json({ err: "no tag list" });
+// 		// tags.push("XXX"); // test: uncomment line, replace XXX with tag of currently live user (it will be the sole "true" response)
+// 		function finishResponse() {
+// 			res.status(200).json({
+// 				tagInfo,
+// 			});
+// 		}
+// 		const tagInfo: { [key: string]: boolean } = {};
+// 		let tagCount = tags.length;
+// 		for (let i in tags) {
+// 			const tag = tags[i];
+// 			if (!tag) {
+// 				tagCount--;
+// 				continue;
+// 			} //ignore blank lines
+// 			tagCheck(tag).then((res) => {
+// 				tagInfo[tag] = res;
+// 				if (Object.keys(tagInfo).length === tagCount) finishResponse();
+// 			});
+// 		}
+// 	});
+// }
